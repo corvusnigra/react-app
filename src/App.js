bd25332b728bbc3998ce417942ab12f6e281030a
corvusnigra <itemruby@gmail.com>
2021-01-27 23:54:51 +0300

    create app template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/HeaderComponent";
+import Footer from "./components/FooterComponent";
+import Layout from "./components/LayoutComponent";
+
+import bg1 from './assets/bg1.jpg'
+import bg3 from './assets/bg3.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello world
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header
+                title='This is title'
+                descr='This is Description!'
+            />
+            <Layout
+               title='Layout 1'
+               desc='description layout 1'
+               id='layout1'
+               urlBg={bg1}
+            />
+            <Layout
+                title='Layout 2'
+                desc='description layout 2'
+                id='layout2'
+                colorBg='green'
+            />
+            <Layout
+                title='Layout 3'
+                desc='description layout 3'
+                id='layout3'
+                urlBg={bg3}
+            />
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
