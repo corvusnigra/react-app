6d6927b1a0478ab3c41dca94ccd3a51ca543234c
corvusnigra <itemruby@gmail.com>
2021-01-30 00:33:39 +0300

    add pokemon collection

--- src/components/LayoutComponent/index.js
+++ src/components/LayoutComponent/index.js
@@ -1,7 +1,7 @@
 import s from './style.module.css'
 
 
-const Layout = ({id, title, desc, colorBg, urlBg }) => {
+const Layout = ({id, title, colorBg, urlBg, children }) => {
     const styleColorBg = colorBg ? {backgroundColor: colorBg} : {};
     const styleUrlBg = urlBg ? {backgroundImage: `url(${urlBg})`} : {};
     return (
@@ -12,8 +12,8 @@ const Layout = ({id, title, desc, colorBg, urlBg }) => {
                         { title && (<h3>{title}</h3>)}
                         <span className={s.separator}></span>
                     </div>
-                    <div className={s.desc}>
-                        {desc && (<p>{desc}</p>)}
+                    <div className={`${s.desc} ${s.full}`}>
+                        {children && (children)}
                     </div>
                 </article>
             </div>
